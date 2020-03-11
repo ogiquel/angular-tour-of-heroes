8f17346ba5f8d002ba364551e2342e786ef724ac
ogiquel <ogiquel@uci.cu>
2020-03-11 14:36:18 -0400

    using angular material

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,26 @@
-import { Injectable } from "@angular/core";
-import { Hero } from "./hero";
-import { Observable, of } from "rxjs";
-import { MessageService } from "./message.service";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { catchError, map, tap } from "rxjs/operators";
+import {Injectable} from '@angular/core';
+import {Hero} from './hero';
+import {Observable, of} from 'rxjs';
+import {MessageService} from './message.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, map, tap} from 'rxjs/operators';
 
 @Injectable({
-    providedIn: "root"
+    providedIn: 'root'
 })
 export class HeroService {
     constructor(
         private http: HttpClient,
         private messageService: MessageService
-    ) {}
+    ) {
+    }
 
     /** Log a HeroService message with the MessageService */
     private log(message: string) {
         this.messageService.add(`HeroService: ${message}`);
     }
-    private heroesUrl = "api/heroes"; // URL to web api
+
+    private heroesUrl = 'api/heroes'; // URL to web api
 
     /**
      * Handle Http operation that failed.
@@ -26,7 +28,7 @@ export class HeroService {
      * @param operation - name of the operation that failed
      * @param result - optional value to return as the observable result
      */
-    private handleError<T>(operation = "operation", result?: T) {
+    private handleError<T>(operation = 'operation', result?: T) {
         return (error: any): Observable<T> => {
             // TODO: send the error to remote logging infrastructure
             console.error(error); // log to console instead
@@ -40,16 +42,17 @@ export class HeroService {
     }
 
     httpOptions = {
-        headers: new HttpHeaders({ "Content-Type": "application/json" })
+        headers: new HttpHeaders({'Content-Type': 'application/json'})
     };
 
     /** GET heroes from the server */
     getHeroes(): Observable<Hero[]> {
         return this.http.get<Hero[]>(this.heroesUrl).pipe(
-            tap(_ => this.log("fetched heroes")),
-            catchError(this.handleError<Hero[]>("getHeroes", []))
+            tap(_ => this.log('fetched heroes')),
+            catchError(this.handleError<Hero[]>('getHeroes', []))
         );
     }
+
     /** GET hero by id. Will 404 if id not found */
     getHero(id: number): Observable<Hero> {
         const url = `${this.heroesUrl}/${id}`;
@@ -63,7 +66,7 @@ export class HeroService {
     updateHero(hero: Hero): Observable<any> {
         return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
             tap(_ => this.log(`updated hero id=${hero.id}`)),
-            catchError(this.handleError<any>("updateHero"))
+            catchError(this.handleError<any>('updateHero'))
         );
     }
 
@@ -75,18 +78,18 @@ export class HeroService {
                 tap(
                     (newHero: Hero) =>
                         this.log(`added hero w/ id=${newHero.id}`),
-                    catchError(this.handleError<Hero>("addHero"))
+                    catchError(this.handleError<Hero>('addHero'))
                 )
             );
     }
 
     /** DELETE: delete the hero from the server */
     deleteHero(hero: Hero | number): Observable<Hero> {
-        const id = typeof hero === "number" ? hero : hero.id;
+        const id = typeof hero === 'number' ? hero : hero.id;
         const url = `${this.heroesUrl}/${id}`;
         return this.http.delete<Hero>(url, this.httpOptions).pipe(
             tap(_ => this.log(`deleted hero id:${id}`)),
-            catchError(this.handleError<Hero>("deleteHero"))
+            catchError(this.handleError<Hero>('deleteHero'))
         );
     }
 
@@ -104,7 +107,7 @@ export class HeroService {
                         x.length
                             ? this.log(`found heroes matching "${term}"`)
                             : this.log(`no heroes matching "${term}"`),
-                    catchError(this.handleError<Hero[]>("searchHeroes", []))
+                    catchError(this.handleError<Hero[]>('searchHeroes', []))
                 )
             );
     }
